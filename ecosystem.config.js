41bb36925983c6aea0ef354577de3cd4cb38ec08
Mike Nikonov <lounge1k@gmail.com>
2018-03-10 17:03:11 +0100

    new deployment

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,11 +6,11 @@ module.exports = {
 	deploy: {
 	  production: {
 		user: 'ubuntu',
-		host: 'ec2-35-180-42-143.eu-west-3.compute.amazonaws.com',
+		host: 'ec2-18-197-196-123.eu-central-1.compute.amazonaws.com',
 		key: '~/.ssh/key.pem',
 		ref: 'origin/master',
 		repo: 'https://github.com/Lounge1k/telegramBot.git',
-		path: '/telegram-bot',
+		path: '/home/ubuntu/telegramBot',
 		'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
 	  }
 	}

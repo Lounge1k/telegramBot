2a2220284838ac33910721226aae7c16c5ad05bf
Mike Nikonov <lounge1k@gmail.com>
2018-03-10 23:14:26 +0100

    setup a bot

--- index.js
+++ index.js
@@ -1,4 +1,34 @@
-const express = require('express')
-const app = express()
-app.use(express.static('public'))
-app.listen(3000, () => console.log('Server running on port 3000'))
+const TOKEN = process.env.TELEGRAM_TOKEN || '52681063:AAGrnlmboAdbvVO1cZaUqoNjlj5eQR1TUSM';
+const url = 'https://ec2-18-197-196-123.eu-central-1.compute.amazonaws.com/';
+const port = 3000;
+
+const TelegramBot = require('../..');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// No need to pass any parameters as we will handle the updates with Express
+const bot = new TelegramBot(TOKEN);
+
+// This informs the Telegram servers of the new webhook.
+bot.setWebHook(`${url}/bot${TOKEN}`);
+
+const app = express();
+
+// parse the updates to JSON
+app.use(bodyParser.json());
+
+// We are receiving updates at the route below!
+app.post(`/bot${TOKEN}`, (req, res) => {
+  bot.processUpdate(req.body);
+  res.sendStatus(200);
+});
+
+// Start Express Server
+app.listen(port, () => {
+  console.log(`Express server is listening on ${port}`);
+});
+
+// Just to ping!
+bot.on('message', msg => {
+  bot.sendMessage(msg.chat.id, 'I am alive!');
+});
